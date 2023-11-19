60c352b094450e5404c76d73bec5b46ef1b84217
Chris Doty <Chris.Doty@ingramcontent.com>
2023-11-18 20:01:20 -0600

    fix: add cli

---
+++ example.js
@@ -0,0 +1,10 @@
+module.exports = {
+  connection: {
+    engine: 'mysql',
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'password',
+    database: 'db',
+    charset: 'utf8'
+  }
+};
--- run.js
+++ run.js
@@ -1,16 +1,50 @@
+const fs = require("fs");
 const { extractSchemas } = require('./index.js');
 
-async function run() {
-  const connection = {
-    host: '127.0.0.1',
-    database: 'db',
-    user: 'root',
-    password: 'password',
-  };
-
-  const result = await extractSchemas(connection);
-
-  console.log(JSON.stringify(result,null,2));
+async function main(options) {
+  const config = require(options.configFile);
+  const result = await extractSchemas(config.connection);
+  if(options.outputFile) {
+    fs.writeFileSync(options.outputFile, JSON.stringify(result,null,2) ,"utf8")
+  } else console.log(JSON.stringify(result,null,2));
 }
 
-run();
+let options = {
+  configFile:"",
+  outputFile:"",
+}
+
+if (process.argv.length === 2) {
+console.error('Expected at least one argument!');
+process.exit(1);
+} else {
+let argv = process.argv;
+
+for(let i=2;i<argv.length;i++) {
+  if(argv[i].substring(0,2)==="--") {
+      let name = argv[i].substring(2);
+      if(options[name]!==undefined) {
+          options[name] = argv[i+1];
+          i++;
+      } else {
+          console.error('Expected a known option');
+          process.exit(1);
+      }
+  }
+}
+}
+
+(async () => {
+try {
+    console.log('\x1b[32m%s\x1b[0m',"Running with options:\n",JSON.stringify(options,null,2));
+    main(options).then(()=>{
+      if(options.debug) console.log("DONE");
+      process.exit(0);
+    },()=>{
+      if(options.debug) console.log("ERROR");
+      process.exit(1);
+    })
+} catch (e) {
+    process.exit(1);
+}
+})();
