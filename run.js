e0ba5213a8e1a4bd5e466ea7d26bd08165f1a7b3
Chris Doty <Chris.Doty@ingramcontent.com>
2023-11-21 21:39:35 -0600

    fix: output consildated init.sql file

--- run.js
+++ run.js
@@ -7,16 +7,24 @@ async function main(options) {
   const result = await extractSchemas(config.connection,options);
 
   if(options.writeSql){
+
+    const content = [];
     if(result[config.connection.database].tables.length>0) {
       // write table sql
       const tablesPath=path.join(process.cwd(),"tables")
       if (!fs.existsSync(tablesPath)){
         fs.mkdirSync(tablesPath);
       }
+      const byName = {};
       result[config.connection.database].tables.forEach(table => {
+        byName[table.name] = table.definition;
         if(options.verbose) console.log("writing",path.join(tablesPath,table.name+".sql"));
         fs.writeFileSync(path.join(tablesPath,table.name+".sql"), table.definition ,"utf8")
       });
+
+      result[config.connection.database].tableOrder.forEach(table => {
+        content.push(byName[table]);
+      })
     }
 
     if(result[config.connection.database].procedures.length>0) {
@@ -27,11 +35,24 @@ async function main(options) {
       }
       result[config.connection.database].procedures.forEach(proc => {
         if(options.verbose) console.log("writing",path.join(proceduresPath,proc.name+".sql"));
+        content.push(proc.definition);
         fs.writeFileSync(path.join(proceduresPath,proc.name+".sql"), proc.definition ,"utf8")
       });
     }
 
+    if(content.length>0) {
+      fs.writeFileSync(path.join(process.cwd(),"init.sql"), content.join('\n\n') ,"utf8");
+    }
   }
+
+  const seedList = [];
+  result[config.connection.database].tableOrder.forEach(table => {
+    let seedfile = path.join(process.cwd(),"seed",table+'.sql');
+    seedList.push(seedfile);
+    if (!fs.existsSync(seedfile)){
+      content.push(fs.readFileSync(seedfile,"utf8"));
+    }
+  })
 
   if(options.outputFile) {
     fs.writeFileSync(path.join(process.cwd(),options.outputFile), JSON.stringify(result,null,2) ,"utf8")
